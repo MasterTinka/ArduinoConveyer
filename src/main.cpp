14819198394d0dcf06e67fa84d3b693d5c723f27
MasterTinka <znamenochick@gmail.com>
2021-02-23 19:54:29 +0600

    Added Serial

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ digitalPinClassOutput conveyer170(51), conveyer36(52), bunker(53), buzzer(49), a
 
 digitalPinClassOutput* conveyers[3] = {&conveyer170, &conveyer36, &bunker};
 
+String message;
+
 void setup() {
   conveyer170.set_HIGH();
   conveyer36.set_HIGH();
@@ -20,6 +22,8 @@ void setup() {
   Timer30Sec.stop();
   Timer15Sec.setInterval(15000);
   Timer15Sec.stop();
+
+  Serial.begin(9600);
 }
 
 
@@ -40,6 +44,7 @@ void accidentReset()
 
 bool buzzerAction()
 {
+  Serial.print("BUZZER STARTED");
   Timer30Sec.start();
   int8_t amount = 0;
   while(amount < 3)
@@ -48,8 +53,17 @@ bool buzzerAction()
     while(!Timer30Sec.isReady())
     {
       delay(1);
-      if(accidentOnConveyer.read_data() == false)
+      if(Serial.available())
       {
+        while (Serial.available())
+        {
+          char recieved_byte = Serial.read();
+          message += recieved_byte;
+        }
+      }
+      if(accidentOnConveyer.read_data() == false || message == "ACCIDENT")
+      {
+        Serial.print("ACCIDENT STARTED");
         accidentReset();
         return false;
       }
@@ -58,11 +72,13 @@ bool buzzerAction()
   }
   buzzer.set_HIGH();
   Timer30Sec.stop();
+  Serial.print("BUZZER GONE");
   return true;
 }
 
 bool startConveyersFunction()
 {
+  Serial.print("CONVEYERS_START STARTED");
   Timer15Sec.start();
   for(int i = 0; i < 3; i++)
   {
@@ -70,37 +86,70 @@ bool startConveyersFunction()
     while(!Timer15Sec.isReady() && i != 2)
     {
       delay(1);
-      if(accidentOnConveyer.read_data() == false)
+      if(Serial.available())
       {
+        while (Serial.available())
+        {
+          char recieved_byte = Serial.read();
+          message += recieved_byte;
+        }
+      }
+      if(accidentOnConveyer.read_data() == false || message == "ACCIDENT")
+      {
+        Serial.print("ACCIDENT STARTED");
         accidentReset();
         return false;
       }
     }
   }
+  Serial.print("CONVEYERS_START GONE");
   Timer15Sec.stop();
   return true;
 }
 
 void loop() {
-  if(accidentOnConveyer.read_data() && startConveyers.read_data())
+
+  while (Serial.available())
   {
+    char recieved_byte = Serial.read();
+    message += recieved_byte;
+  }
+
+  if((accidentOnConveyer.read_data() && startConveyers.read_data()) || (message == "START"))
+  {
+    message = "";
+    Serial.println("CONVEYERS STARTED");
     if(buzzerAction() == false)
     {
+      message = "";
       return;
     }
     if(startConveyersFunction() == false)
     {
+      message = "";
       return;
     }
     while (true)
     {
-      if(stopConveyers.read_data())
+      if(Serial.available())
       {
+        while (Serial.available())
+        {
+          char recieved_byte = Serial.read();
+          message += recieved_byte;
+        }
+      }
+      if(stopConveyers.read_data() || message == "STOP")
+      {
+        message = "";
+        Serial.print("CONVEYERS GONE");
         resetConveyers();
         break;
       }
-      else if(accidentOnConveyer.read_data() == false)
+      else if(accidentOnConveyer.read_data() == false || message == "ACCIDENT") 
       {
+        Serial.print("ACCIDENT STARTED");
+        message = "";
         accidentReset();
         break;
       }
@@ -108,6 +157,7 @@ void loop() {
   }
   else if(accidentOnConveyer.read_data() && accident.get_status() == false)
   {
+    Serial.print("ACCIDENT GONE");
     accident.set_HIGH();
   }
 }
